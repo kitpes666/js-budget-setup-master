c09cffe6f802990dcf18d43f7d96a80f79aca10a
kitpes66666 <kitpes66666@gmail.com>
2019-07-19 10:52:34 +0300

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,49 @@
+class UI {
+  constructor() {
+    this.budgetFeedback = document.querySelector(".budget-feedback");
+    this.expenseFeedback = document.querySelector(".expense-feedback");
+    this.budgetForm = document.getElementById("budget-form");
+    this.budgetInput = document.getElementById("budget-input");
+    this.budgetAmount = document.getElementById("budget-amount");
+    this.expenseAmount = document.getElementById("expense-amount");
+    this.balance = document.getElementById("balance");
+    this.balanceAmount = document.getElementById("balance-amount");
+    this.expenseForm = document.getElementById("expense-form");
+    this.expenseInput = document.getElementById("expense-input");
+    this.amountInput = document.getElementById("amount-input");
+    this.expenseList = document.getElementById("expense-list");
+    this.itemList = [];
+    this.itemID = 0;
+  }
+  //submit budget method
+  submitBudgetForm(){
+    console.log('Hello from es6');
+  }
+}
+
+function eventListenters(){
+  const budgetForm = document.getElementById('Budget-form');
+  const expenseForm = document.getElementById('Expense-form');
+  const expenseList = document.getElementById('Expense-List');
+
+//new istance of UI class
+const ui = new UI()
+
+//budget form submit
+budgetForm.addEventListener('submit', function(event){
+  event.preventDefault();
+  ui.submitBudgetForm();
+})
+//expense form submit
+expenseForm.addEventListener('submit', function(event){
+  event.preventDefault();
+})
+//expense click
+expenseList.addEventListener('click', function(){
+})
+}
+
+
+document.addEventListener('DOMContentLoaded', function(){
+  eventListeners();
+})
